a3efac5878f178da9e4d6a23b52d6dfeb513f11e
KogamaProxy <104033723+KogamaProxy@users.noreply.github.com>
2022-04-19 23:25:10 +0200

    Update cheat-proxy.user.js

--- cheat-proxy.user.js
+++ cheat-proxy.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name				  KoGaMa Proxy Cheat
-// @run-at			  document-start
-// @version			  1.0
-// @description	  KoGaMa Injector
-// @author			  none
-// @match				  https://www.kogama.com/page/webgl-frame/*
-// @match				  https://kogama.com.br/page/webgl-frame/*
-// @match				  https://friends.kogama.com/page/webgl-frame/*
+// @name          KoGaMa Proxy Cheat
+// @run-at        document-start
+// @version       1.0
+// @description   KoGaMa Injector
+// @author        none
+// @match         https://www.kogama.com/page/webgl-frame/*
+// @match         https://kogama.com.br/page/webgl-frame/*
+// @match         https://friends.kogama.com/page/webgl-frame/*
 // @require       https://github.com/KogamaProxy/CheatProxy/blob/main/source.js
 // @require       https://pastebin.com/raw/NWL34qG0
-// @grant				  none
+// @grant         none
 // ==/UserScript==
